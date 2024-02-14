cbf35e896056799edad1cd9abed2b45b70bf8f5d
Xin Deng <xind1470@gmail.com>
2024-02-14 13:08:10 -0800

    added demo tests and updated to snack

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,31 +1,77 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
+
 from .models import Snack
 
-class ThingTests(TestCase):
-    
-    def test_list_page_status_code(self):
-        url = reverse("snack_list")
-        response = self.client.get(url)
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester", email="tester@email.com", password="pass"
+        )
+
+        self.snack = Snack.objects.create(name="pizza", rating=1, reviewer=self.user)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), "pizza")
+
+    def test_snack_content(self):
+        self.assertEqual(f"{self.snack.name}", "pizza")
+        self.assertEqual(f"{self.snack.reviewer}", "tester")
+        self.assertEqual(self.snack.rating, 1)
+
+    def test_snack_list_view(self):
+        response = self.client.get(reverse("snack_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "pizza")
+        self.assertTemplateUsed(response, "snack_list.html")
+
+    def test_snack_detail_view(self):
+        response = self.client.get(reverse("snack_detail", args="1"))
+        no_response = self.client.get("/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Purchaser: tester")
+        self.assertTemplateUsed(response, "snack_detail.html")
+
+    def test_snack_create_view(self):
+        response = self.client.post(
+            reverse("snack_create"),
+            {
+                "name": "pizza",
+                "description": "type of snack",
+                "Purchaser": self.user.id,
+            },
+            follow=True,
+        )
+
+        self.assertRedirects(response, reverse("snack_detail", args="2"))
+        self.assertContains(response, "pizza")
+
+    def test_snack_update_view_redirect(self):
+        response = self.client.post(
+            reverse("snack_update", args="1"),
+            {"name": "Updated name", "description": 'type of meal', "purchaser": self.user.id},
+        )
+
+        self.assertRedirects(
+            response, reverse("snack_detail", args="1"), target_status_code=200
+        )
+
+    def test_snack_update_bad_url(self):
+        response = self.client.post(
+            reverse("snack_update", args="9"),
+            {"name": "Updated name", "description": 'type of meal', "reviewer": self.user.id},
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_snack_delete_view(self):
+        response = self.client.get(reverse("snack_delete", args="1"))
         self.assertEqual(response.status_code, 200)
 
-    def test_list_page_template(self):
-        url = reverse("snack_list")
-        response = self.client.get(url)
-        self.assertTemplateUsed(response, "snack_list.html")
-        self.assertTemplateUsed(response, "base.html")
-
-    def setUp(self):
-        purchaser = get_user_model().objects.create(username="test", password = "uncommon")
-        Snack.objects.create(name="pizza", description="type of snack",purchaser=purchaser)
-
-    def test_list_page_context(self):
-        url = reverse('snack_list')
-        response = self.client.get(url)
-        snacks = response.context['object_list']
-        self.assertEqual(len(snacks),1)
-        self.assertEqual(snacks[0].name, "pizza")
-        self.assertEqual(snacks[0].description, "type of snack")
-        self.assertEqual(snacks[0].purchaser.username,"test")
-
+    # you can also tests models directly
+    def test_model(self):
+        snack = Snack.objects.create(name="pizza", purchaser=self.user)
+        self.assertEqual(snack.name, "pizza")
